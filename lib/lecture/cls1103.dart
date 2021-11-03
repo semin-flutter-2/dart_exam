697021f1b9552b1d981c77ef2f2a4971d63eae74
Junsuk Oh <a811219@gmail.com>
2021-11-03 12:48:03 +0900

    수업 내용 업로드

--- lib/lecture/cls1103.dart
+++ lib/lecture/cls1103.dart
@@ -30,11 +30,20 @@ $address 에 아래의 메일을 송신한다.
 }
 
 void introduceOneself() {
+  // 용사
   String name = '오준석';
   int age = 41;
   double height = 174.0;
   String gender = '남';
 
   print('저는 $name입니다. $age살 이고요, 키는 ${height}Cm이고 $gender자입니다');
+
+  // 나쁜놈1
+  String name2 = '오준석';
+  int age2 = 41;
+  double height2 = 174.0;
+  String gender2 = '남';
+
+  print('저는 $name2입니다. $age2살 이고요, 키는 ${height2}Cm이고 $gender2자입니다');
 }
 
