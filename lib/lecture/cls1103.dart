5a4b567c63b8fffe3b435a145b9296d3ca9a0471
Junsuk Oh <a811219@gmail.com>
2021-11-03 09:46:10 +0900

    메서드 작성 연습 1

---
+++ lib/lecture/cls1103.dart
@@ -0,0 +1,14 @@
+void main() {
+  introduceOneself();
+}
+
+
+
+void introduceOneself() {
+  String name = '오준석';
+  int age = 41;
+  double height = 174.0;
+  String gender = '남';
+
+  print('저는 $name입니다. $age살 이고요, 키는 ${height}Cm이고 $gender자입니다');
+}
