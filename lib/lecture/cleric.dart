0748e130f4dafa6264180abc186e774f2c9213fa
Junsuk Oh <a811219@gmail.com>
2021-11-04 09:52:33 +0900

    클레릭 작성

--- lib/lecture/cleric.dart
+++ lib/lecture/cleric.dart
@@ -1,7 +1,24 @@
 void main() {
-  final cleric = Cleric(name: '이름', hp: 100, mp: 50);
+  // 타입 변수명 = 값
+  // int i = 10;
+  // Cleric타입 cleric이름으로 = 클래릭을 만들자
+  Cleric cleric = Cleric(name: '이름', hp: 100, mp: 50);
+  final cleric2 = Cleric(name: '이름');
+  final cleric3 = Cleric();
+
+  print(cleric.hp);
 }
 
 class Cleric {
+  String name;
+  int hp;
+  int mp;
+  final int maxHp = 50;
+  final int maxMp = 10;
 
+  Cleric({this.name, this.hp = 50, this.mp = 10});
+
+  void selfAid() {
+
+  }
 }
